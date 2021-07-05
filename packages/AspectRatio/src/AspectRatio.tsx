e49c1847dc5ba3903f19474f4fab181bbc024bce
limichange <limichange@hotmail.com>
2021-07-05 23:13:44 +0800

    chore: add AspectRatio empty folder

---
+++ packages/AspectRatio/src/AspectRatio.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Props = any
+
+const AspectRatio: React.FC<Props> = () => {
+  return <div>AspectRatio</div>
+}
+
+export default AspectRatio
